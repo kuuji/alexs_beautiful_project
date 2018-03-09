98046c794fe07ff12ad28153fa82f7d617f6a509
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 13:56:01 -0500

    allow all host

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,8 +9,7 @@ import os
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='MJY4BSgAxtBhdHxHmMxKXZH2UEr8GCe9WUBekcfhfy35JrSZDy')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
+    "*",
 ]
 
 # CACHES

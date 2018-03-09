afb07c880e8c0003a19909d75d6f5fe59ac4fdf0
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 11:04:14 -0500

    disable prompt gcloud sdk

--- before_install.sh
+++ before_install.sh
@@ -5,7 +5,8 @@ IFS=$'\n\t'
 go get github.com/openshift/source-to-image/cmd/s2i
 
 if [ ! -d ${HOME}/google-cloud-sdk ]; then
-     curl https://sdk.cloud.google.com | bash;
+     export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+     curl https://sdk.cloud.google.com | bash ;
 fi
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json

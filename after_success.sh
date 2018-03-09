502daad34a8d4ae673ce6d8aa9e6fa325102215f
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 12:55:22 -0500

    im stoupid

--- after_success.sh
+++ after_success.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-set -euov pipefail
+set -euo pipefail
 IFS=$'\n\t'
 
  coveralls
 if [ "$TRAVIS_BRANCH" != "master" ]; then
   s2i build . centos/python-36-centos7 us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_BRANCH
   gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_BRANCH
-elif [ ! -z $TRAVIS_TAG ]; then
-  echo "s2i build"
-  # s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_TAG
-  echo "push to gcloud registry"
-  # gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_TAG
+fi
+
+if [ ! -z $TRAVIS_TAG ]; then
   echo "deploy to k8s"
   kubectl config view
   kubectl config current-context

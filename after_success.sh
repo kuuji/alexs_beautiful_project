c0ad001bae7f4cfae42957c87c287e7d9960bbe0
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 10:06:33 -0500

    add s2i

---
+++ after_success.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+coveralls
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_BRANCH
+elif [ -z $TRAVIS_TAG ]; then
+  s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_TAG
+fi

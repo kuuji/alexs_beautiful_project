158f4e52ffe416a54402f57ea285ff66b72543c2
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 12:49:36 -0500

    remove build and push

--- after_success.sh
+++ after_success.sh
@@ -8,9 +8,9 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
   gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_BRANCH
 elif [ ! -z $TRAVIS_TAG ]; then
   echo "s2i build"
-  s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_TAG
+  # s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_TAG
   echo "push to gcloud registry"
-  gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_TAG
+  # gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_TAG
   echo "deploy to k8s"
   kubectl config view
   kubectl config current-context

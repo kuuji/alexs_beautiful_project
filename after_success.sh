9f2c3a52cf9a2621fdb77e31eff8aa9754f5ca7a
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 12:12:37 -0500

    move stuff to script for more clarity

--- after_success.sh
+++ after_success.sh
@@ -2,15 +2,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# coveralls
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-  s2i build . centos/python-36-centos7 us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_BRANCH
-  gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_BRANCH
-elif [ ! -z $TRAVIS_TAG ]; then
-  echo "s2i build"
-  s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_TAG
-  echo "push to gcloud registry"
-  gcloud docker -- push us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_TAG
-  echo "deploy to k8s"
-  kubectl set image deployment/alexs_beautiful_project alexs_beautiful_project=us.gcr.io/alexs_beautiful_project/alexs_beautiful_project:$TRAVIS_TAG
-fi
+

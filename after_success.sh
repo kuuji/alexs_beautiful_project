785442968c34e9b45776d92022822c395af46ab5
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 10:24:30 -0500

    bashism

--- after_success.sh
+++ after_success.sh
@@ -7,6 +7,6 @@ echo $TRAVIS_BRANCH
 echo $TRAVIS_TAG
 if [ "$TRAVIS_BRANCH" != "master" ]; then
   s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_BRANCH
-elif [ -z $TRAVIS_TAG ]; then
+elif [ ! -z $TRAVIS_TAG ]; then
   s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_TAG
 fi

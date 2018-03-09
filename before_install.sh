7c7af5fce014c7ff2ac14416919feae5547d4d08
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 11:38:37 -0500

    remove update beta

--- before_install.sh
+++ before_install.sh
@@ -11,7 +11,6 @@ fi
 
 source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud --quiet components update
-gcloud --quiet components beta update
 gcloud --quiet components update kubectl
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json

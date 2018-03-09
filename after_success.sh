4be72932c328f2d8328162db4b9c0ae668a26a84
Alexandre Garcia <alexandre@vmfarms.com>
2018-03-09 10:14:28 -0500

    test with s2i

--- after_success.sh
+++ after_success.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 coveralls
-
+echo $TRAVIS_BRANCH
+echo $TRAVIS_TAG
 if [ "$TRAVIS_BRANCH" != "master" ]; then
   s2i build . centos/python-36-centos7 alexs_beautiful_project:$TRAVIS_BRANCH
 elif [ -z $TRAVIS_TAG ]; then
